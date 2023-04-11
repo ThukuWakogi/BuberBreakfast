f4fd204edd1990212b3826fca524f6cb0baece1f
Oliver Timothy <oliverwakogi@live.com>
2023-04-11 14:18:11 +0300

    :wrench:: Add business logic

---
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -0,0 +1,31 @@
+﻿using BuberBreakfast.Contracts.Breakfast;
+using Microsoft.AspNetCore.Mvc;
+namespace BuberBreakfast.Controllers;
+
+[ApiController]
+public class BreakfastController : ControllerBase
+{
+    [HttpPost("/breakfasts")]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("/breakfasts/{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPut("/breakfasts/{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpDelete("/breakfasts/{id:guid}")]
+    public IActionResult DeleteGetBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+}
